070e918a5e7a02a0604c1882d4becd21d7d9efa4
VladimirRenaudin <vladimir.renaudin@hotmail.fr>
2020-10-19 10:42:02 +0200

    first version of help

--- computor.py
+++ computor.py
@@ -27,11 +27,23 @@ elif len(sys.argv) > 2:
             natural = True
         elif elem == '-g':
             graph = True
+elif len(sys.argv) == 2 and sys.argv[1] == '-h':
+    display_help = True
 
-eq = parse_equation(sys.argv[1])
-print_reduced_form(eq.reduced_elements, natural)
-print_degree(eq.reduced_elements)
-solve(eq.reduced_elements, verbose)
-
-if graph:
-    poly_graph(eq.reduced_elements)
+if display_help:
+    print("Utilisation de computor : ")
+    print('\t"python3 computor.py equation options"')
+    print('\tequation de la forme : "c*X^0 + c*X^1 + a*X^2 = f*X^0 + e*X^1 + d*X^2"')
+    print('\toptions :')
+    print('\t\t-h: display the help')
+    print('\t\t-v: display the details of the maths')
+    print('\t\t-n: display reduced form as natural writing')
+    print('\t\t-g: display a graph of the equation')
+else:
+    eq = parse_equation(sys.argv[1])
+    print_reduced_form(eq.reduced_elements, natural)
+    print_degree(eq.reduced_elements)
+    solve(eq.reduced_elements, verbose)
+    
+    if graph:
+        poly_graph(eq.reduced_elements)
